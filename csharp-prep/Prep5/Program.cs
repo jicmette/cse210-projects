e2ef0037fd348b43e1d1136dcb86e591efb4c9eb
jicmette <jicmettesc@gmail.com>
2023-01-11 23:27:59 -0600

    W02 Functions

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string userName = PromptUserName();
+        int favNumber = PromptUserNumber();
+
+        int squareN = SquareNumber(favNumber);
+        DisplayResult(userName, squareN);
+    }
+
+
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string userName = Console.ReadLine();
+        return userName;
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        int favNumber = int.Parse(Console.ReadLine());
+        return favNumber;
+    }
+
+    static int SquareNumber(int favNumber)
+    {
+        int squareN = favNumber * favNumber;
+
+        return squareN;
+    }
+
+    static void DisplayResult(string userName, int squareN)
+    {
+        Console.WriteLine($"{userName}, the square of your number is {squareN}");
     }
 }
