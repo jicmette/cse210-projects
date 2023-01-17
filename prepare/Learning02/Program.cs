9dd70531bca2e061158bb9b9354eec37d331ca90
jicmette <jicmettesc@gmail.com>
2023-01-16 19:45:29 -0600

    W02 Abstraction Learning Activity

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Administrative Assitant";
+        job1._company = "Blag Media";
+        job1._startYear = 2018;
+        job1._endYear = 2021;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Information Technology Technical Support";
+        job2._company = "Nexbitek IT Services";
+        job2._startYear = 2021;
+        job2._endYear = 2023;
+
+        Resume myResume = new Resume();
+        myResume._name = "Israel Carmona";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.DisplayJobDetails();
     }
+
+
+
+
 }
