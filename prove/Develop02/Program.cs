828568f6946b395b0fe892603d8b61ae8bc87b65
jicmette <jicmettesc@gmail.com>
2023-02-06 21:16:15 -0600

    Journal Project

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -7,11 +7,54 @@ class Program
     static void Main(string[] args)
 
     {
-        Menu menu = new Menu();
-        menu.DisplayMenu();
 
-        Entry entries = new Entry();
-        entries.EntryCWR();
+        int choice1 = 0;
+        Prompt prompts = new Prompt();
+        Files filePath = new Files();
+
+        do
+        {
+            Console.WriteLine("Please select one of the following choices:");
+
+            List<string> listOptions = new List<string>()
+            {
+            "1. Write", "2. Display", "3. Load", "4. Save", "5. Quit"
+            };
+
+            for (int i = 0; i < listOptions.Count; i++)
+                Console.WriteLine(listOptions[i]);
+
+            Console.WriteLine("What would you like to do? (Choose a number between 1 to 5): ");
+            string choiceInput = Console.ReadLine();
+            choice1 = int.Parse(choiceInput);
+
+            if (choice1 == 1)
+            {
+                prompts.DisplayQuestion();
+                filePath.FileCreation();
+            }
+
+            else if (choice1 == 2)
+            {
+                Console.WriteLine("Display");
+            }
+
+            else if (choice1 == 3)
+            {
+                Console.WriteLine("Load");
+            }
+
+            else if (choice1 == 4)
+            {
+                Console.WriteLine("Save");
+            }
+            else
+            {
+                Console.WriteLine("See you soon!");
+            }
+
+
+        } while (choice1 != 5);
     }
 }
 
--- prove/Develop02/Prompt.cs
+++ prove/Develop02/Prompt.cs
@@ -1,9 +1,10 @@
 using System;
 using System.IO;
 using System.Text;
+using System.Collections.Generic;
 
 
-class Prompt
+public class Prompt
 {
     
     Random rnd = new Random();
@@ -16,11 +17,19 @@ class Prompt
             "If I had one thing I could do over today, what would it be?",
         };
 
+    public List<string> entries = new List<string>();
+
     public void DisplayQuestion()
     {
+        Files file = new Files();
+
         int index = rnd.Next(0, 5);
         Console.WriteLine(questions[index]);
         string entry1 = Console.ReadLine();
+        DateTime theCurrentTime = DateTime.Now;
+        string dateText = theCurrentTime.ToShortDateString();
+        entries.Add($"Date: {dateText} - {entry1}");
+        Console.WriteLine("Entry Saved!");
 
     }
 }
