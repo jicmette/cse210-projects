6d85aca06c33952684db2dc224b8b58b7acb7e9e
Israel Carmona <jicmettesc@gmail.com>
2023-03-07 18:01:44 -0600

    Develop05 folder added

---
+++ prove/Develop05/Goals.cs
@@ -0,0 +1,88 @@
+using System;
+
+public class Goals
+{
+
+    private string _name;
+    private string _description;
+    private int _points;
+    //private List<string> goals;
+
+    // public Goals(string name, string description, int points)
+    // {
+    //     _name = name;
+    //     _description = description;
+    //     _points = points;
+
+    // }
+
+    public void MenuTypesGoal()
+    {   
+        Console.WriteLine();
+        Console.WriteLine("The types of goals are:");
+        List<string> _typesGoals = new List<string>()
+        {
+            "1. Simple Goal", 
+            "2. Eternal Goal", 
+            "3. Checklist Goal"
+        };
+
+        for (int l = 0; l < _typesGoals.Count; l++)
+            Console.WriteLine(_typesGoals[l]);
+            Console.Write("Which type of goal would you like to create? ");
+            string _choiceUser = Console.ReadLine();
+            int _choiceUSerInt = int.Parse(_choiceUser);
+            Console.WriteLine();
+
+            if (_choiceUSerInt == 1)
+            {
+                QuestionsGoals();
+            }
+
+            else if (_choiceUSerInt == 2)
+            {
+                QuestionsGoals();
+            }
+
+            else if (_choiceUSerInt == 3)
+            {
+                QuestionsGoals();
+                QuestionsGoalChecklist();
+            }
+
+    }
+
+    public void QuestionsGoals()
+    {
+        Console.WriteLine("What is the name of your goal?");
+        string _name = Console.ReadLine();
+        Console.WriteLine("What is a short description of it?");
+        string _description = Console.ReadLine();
+        Console.WriteLine("What is the amount of points associated with this goal?");
+        string _pointString = Console.ReadLine();
+        int _points = int.Parse(_pointString);
+        
+
+    }
+
+    public void QuestionsGoalChecklist()
+    {
+        Console.WriteLine("How many times does this goal need to be accomplished for a bonus?");
+        string _times = Console.ReadLine();
+        int _timesAccom = int.Parse(_times);
+        Console.WriteLine("What is the bonus for accomplishing it that many times?");
+        string _bonus = Console.ReadLine();
+        int _bonusAdded = int.Parse(_bonus);
+        
+        
+
+    }
+
+
+}
+
+
+
+
+
+
